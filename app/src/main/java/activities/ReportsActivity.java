5ce46f38e9262ce16894c65bb29e1f9a323ce980
Michael Brown <mikeabrownjr+github@gmail.com>
2022-12-19 18:49:14 -0500

    Added a new ReportActivity page to get a list of items of the student's account such as the total number of terms, courses, and the current date and time.

--- app/src/main/java/activities/ReportsActivity.java
+++ app/src/main/java/activities/ReportsActivity.java
@@ -1,4 +1,64 @@
 package activities;
 
-public class ReportsActivity {
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.mbro.wguapp.R;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+import adapters.CourseAdapter;
+import adapters.TermAdapter;
+import viewmodel.CourseViewModel;
+import viewmodel.TermViewModel;
+
+public class ReportsActivity extends AppCompatActivity {
+    private TermAdapter adapterTerm = new TermAdapter();
+    private CourseAdapter adapterCourse = new CourseAdapter();
+    private Date date;
+    private TermViewModel termViewModel;
+    private CourseViewModel courseViewModel;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_report);
+
+        // Setting the Date
+        date = new Date();
+        SimpleDateFormat format = new SimpleDateFormat("MM-dd-yyyy HH:mm:ss");
+        String formattedDate = format.format(date);
+
+        // Setting the Terms
+        TextView textViewTotalTerms = findViewById(R.id.total_terms);
+        TextView textViewNumTerms = findViewById(R.id.number_of_terms);
+
+        termViewModel = new ViewModelProvider(this).get(TermViewModel.class);
+        termViewModel.getAllTerms().observe(this, terms->{
+            adapterTerm.setTerms(terms);
+        });
+
+        int numTerms = adapterTerm.getItemCount();
+        textViewNumTerms.setText(String.valueOf(numTerms));
+
+        TextView textViewTotalCourses = findViewById(R.id.total_courses);
+        TextView textViewNumCourses = findViewById(R.id.number_of_courses);
+
+//        int numCourses = adapterCourse.getItemCount();
+//        textViewNumCourses.setText(String.valueOf(numCourses));
+
+        TextView textViewCurrentDate = findViewById(R.id.current_date);
+        TextView textViewDate = findViewById(R.id.date);
+        textViewDate.setText(formattedDate);
+
+
+
+    }
 }
